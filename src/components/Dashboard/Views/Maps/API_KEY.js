2feb40b4a8d771c5d5174da99d3916b27ec94639
Wang Yu <dalba1992@yandex.com>
2018-03-06 03:30:08 -0400

    signin

---
+++ src/components/Dashboard/Views/Maps/API_KEY.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'AIzaSyB2rY0wtTg9-j5P7rAt_k1X-dgW_fCdv8U'
---
+++ src/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import "babel-polyfill";
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import axios from 'axios'
+import store from './store/store'
+
+// LightBootstrap plugin
+import LightBootstrap from './light-bootstrap-main'
+
+// router setup
+import routes from './routes/routes'
+// plugin setup
+Vue.use(VueRouter)
+Vue.use(LightBootstrap)
+
+// configure router
+const router = new VueRouter({
+  routes:routes, // short for routes: routes
+  linkActiveClass: 'nav-item active'
+})
+
+axios.defaults.baseURL = 'https://troove-168621.firebaseio.com/'
+// axios.defaults.headers.common['Authorization'] = 'fasfdsa'
+axios.defaults.headers.get['Accepts'] = 'application/json'
+
+const reqInterceptor = axios.interceptors.request.use(config => {
+  console.log('Request Interceptor', config)
+  return config
+})
+const resInterceptor = axios.interceptors.response.use(res => {
+  console.log('Response Interceptor', res)
+  return res
+})
+
+axios.interceptors.request.eject(reqInterceptor)
+axios.interceptors.response.eject(resInterceptor)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App),
+  store
+})
